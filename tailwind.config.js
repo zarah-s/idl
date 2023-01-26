fc317513007cf279b625082964018c01c73987aa
zarah <devvick230385@gmail.com>
2023-01-26 18:19:20 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      xs: "320px",
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+    },
+  },
+  plugins: [],
+};
